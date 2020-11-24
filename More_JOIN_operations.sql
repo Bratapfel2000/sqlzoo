d0e93a0c3941f87b2592a96e6fdd1541e1b5796c
bratapfel <bratapfel2k@gmail.com>
2020-11-24 07:55:49 +0100

    added 12.-15. in morejoinoperations

--- More_JOIN_operations.sql
+++ More_JOIN_operations.sql
@@ -55,7 +55,32 @@ WHERE name='Rock Hudson'
 GROUP BY yr
 HAVING COUNT(title) > 2
 
+12.Lead actor in Julie Andrews movies
+SELECT title, name
+FROM movie JOIN casting ON (movieid=movie.id AND ord=1)
+JOIN actor ON (actorid=actor.id)
+WHERE movie.id IN (
+  SELECT movieid FROM casting
+WHERE actorid IN (
+SELECT id FROM actor
+  WHERE name='Julie Andrews'))
+  
+13.Actors with 15 leading roles
+SELECT name FROM actor
+  JOIN casting ON (id = actorid AND (SELECT COUNT(ord) FROM casting WHERE actorid = actor.id AND ord=1)>=15)
+GROUP BY name
 
+14.List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+SELECT title, Count(actorid)
+ FROM casting JOIN movie ON movie.id = casting .movieid
+ WHERE movie .yr = 1978
+GROUP BY movie .title
+ORDER BY Count(casting .actorid) DESC, title
+
+15.List all the people who have worked with 'Art Garfunkel'.
+SELECT DISTINCT name
+FROM actor JOIN casting ON id=actorid
+WHERE movieid IN (SELECT movieid FROM casting JOIN actor ON (actorid=id AND name='Art Garfunkel')) AND name != 'Art Garfunkel'
 
 
 
