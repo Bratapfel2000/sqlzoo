bbb4890c80fb9c154e2e047045f6036273d7e1d4
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-11-26 19:22:11 +0100

    Update Using_Null.sql

--- Using_Null.sql
+++ Using_Null.sql
@@ -16,6 +16,14 @@ SELECT teacher.name, dept.name
 FROM teacher RIGHT JOIN dept
           ON (teacher.dept=dept.id)
 
-5.Using the COALESCE function
+5.Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
 SELECT name ,COALESCE(mobile,'07986 444 2266') AS aff
   FROM teacher 
+
+6.Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name, 'None')
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+     
+7.Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(name), COUNT(mobile) FROM teacher;
